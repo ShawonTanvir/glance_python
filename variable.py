9390e4ef49de8a6d314f86f64aabdbd88bcb7437
ShawonTanvir <143434101+ShawonTanvir@users.noreply.github.com>
2023-12-07 03:28:25 +0600

    Update variable.py
    Add string shortly.

--- variable.py
+++ variable.py
@@ -1,9 +1,16 @@
-'''Lets first talk about the topic we are going to discuss and that is variable. 
-If we break down into two pices of words then we will get vary and able .
+'''Let's first talk about the topic we are going to discuss, and that is variable. 
+If we break it down into two pieces of words, then we will get vary and able.
 We are quite familiar with "vary" word because of math class. From math class, we know that
-for intance x can be hold any value such 4 or 5 or 6 whatever the value is. Similary in programming 
-a variable is  able to vary or change. it could be number , name etc. 
+for instance, x can hold any value, such as 4, 5, or 6 whatever the value is. Similarly in programming 
+a variable is  able to vary or change. It could be a number, name, etc. 
 ''' 
 #number variable 
 number = 69
-print(number) # print is a function which operate to show a value as a output
+# print is a function that shows a value as an output.
+print(number) 
+
+''' So to speak, there are sorts of values that can be stored in a variable. The string is one of them, which is made up of characters
+and can include letters, words, phrases, or symbols. It is written inside parentheses with a double quotation or a single quotation.
+'''
+name = "peter parker"
+print(name) # To show you peter parker as an output on your screen exactly what you are writing inside this parentheses
