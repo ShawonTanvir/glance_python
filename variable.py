c08398d13855eb059b47de0b4833db7fd25602ac
ShawonTanvir <shawontanvir1961@gmail.com>
2023-12-07 03:01:13 +0600

    learn variable

---
+++ variable.py
@@ -0,0 +1,9 @@
+'''Lets first talk about the topic we are going to discuss and that is variable. 
+If we break down into two pices of words then we will get vary and able .
+We are quite familiar with "vary" word because of math class. From math class, we know that
+for intance x can be hold any value such 4 or 5 or 6 whatever the value is. Similary in programming 
+a variable is  able to vary or change. it could be number , name etc. 
+''' 
+#number variable 
+number = 69
+print(number) # print is a function which operate to show a value as a output
